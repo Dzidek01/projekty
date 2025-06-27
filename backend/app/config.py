c0bd10a5505252f9cbf5404627517801cd1d08cf
Rega <93872094+RegaVH@users.noreply.github.com>
2025-06-27 14:01:32 +0200

    Update config.py

--- backend/app/config.py
+++ backend/app/config.py
@@ -4,6 +4,6 @@ from dotenv import load_dotenv
 load_dotenv()
 
 class Config:
-    SQLALCHEMY_DATABASE_URI = os.getenv('DB_URL','postgresql://postgres:admin@localhost:8081/hotel')
+    SQLALCHEMY_DATABASE_URI = os.getenv('')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    JWT_SECRET_KEY = os.getenv('JWT_SECRET','tajny-klucz')
+    JWT_SECRET_KEY = os.getenv('JWT_SECRET','')
